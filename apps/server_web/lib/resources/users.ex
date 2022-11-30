131f8572e9ab97c69a74e2e1dae20348eeb022ce
Gabriel Lopes Veiga <veigo@veigo.dev>
2022-11-30 01:27:44 -0300

    validate username & password
    other fields still can be any type

--- apps/server_web/lib/resources/users.ex
+++ apps/server_web/lib/resources/users.ex
@@ -44,15 +44,23 @@ defmodule Web.Resources.Users do
   end
 
   defp post3(req, state, username, password) do
+    cond do
+      not is_binary(username) -> {false, error_username_not_string(req), state}
+      not is_binary(password) -> {false, error_password_not_string(req), state}
+      true -> post4(req, state, username, password)
+    end
+  end
+
+  defp post4(req, state, username, password) do
     result = Core.User.register(username, password)
 
     case result do
-      :ok -> post4(req, state, username)
+      :ok -> post5(req, state, username)
       {:error, reason} -> {false, error_register(req, reason), state}
     end
   end
 
-  def post4(req, state, username) do
+  def post5(req, state, username) do
     base_url = Application.fetch_env!(:server_web, :base_url)
     resource_url = Path.join([base_url, "/users/", username])
     {{:created, resource_url}, req, state}
@@ -65,6 +73,16 @@ defmodule Web.Resources.Users do
 
   defp error_invalid_data(req) do
     map = %{"error" => "Invalid data. Expected username and password."}
+    :cowboy_req.set_resp_body(Jason.encode!(map), req)
+  end
+
+  defp error_username_not_string(req) do
+    map = %{"error" => "Username must be a string."}
+    :cowboy_req.set_resp_body(Jason.encode!(map), req)
+  end
+
+  defp error_password_not_string(req) do
+    map = %{"error" => "Password must be a string."}
     :cowboy_req.set_resp_body(Jason.encode!(map), req)
   end
 
